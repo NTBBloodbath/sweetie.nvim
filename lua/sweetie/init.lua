0f7b12d0e02f2a0ee25b472de8ed7558ca4942da
NTBBloodbath <bloodbathalchemist@protonmail.com>
2023-08-27 19:43:43 -0400

    hotfix: merge defaults with user configurations

--- lua/sweetie/init.lua
+++ lua/sweetie/init.lua
@@ -8,8 +8,12 @@ local sweetie = {}
 local default_config = require("sweetie.config")
 local highlights = require("sweetie.highlights")
 
+-- fallback to default configuration values if there is no user configuration yet
+-- and merge defaults with user configuration values otherwise
 if not vim.g.sweetie or vim.g.sweetie <= 0 then
   vim.g.sweetie = default_config
+else
+  vim.g.sweetie = vim.tbl_deep_extend("force", default_config, vim.g.sweetie)
 end
 
 sweetie.set = function()
